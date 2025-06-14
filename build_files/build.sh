d082628a7fabe4408ef22ddea82070cfc46476c1
large-farva <148006495+large-farva@users.noreply.github.com>
2025-06-14 14:01:58 -0700

    build.sh 10

--- build_files/build.sh
+++ build_files/build.sh
@@ -100,19 +100,6 @@ enable-hot-corners=false
 favorite-apps=['org.mozilla.firefox.desktop','steam.desktop','org.gnome.TextEditor.desktop','org.gnome.Nautilus.desktop','org.gnome.Calculator.desktop','gnome-software.desktop','gnome-control-center.desktop']
 enabled-extensions=['dash-to-panel@jderose9.github.com','app-hider@lynith.dev','extension-list@tu.berry','vitals@CoreCoding.com','appindicatorsupport@rgcjonas.gmail.com','blur-my-shell@aunetx','caffeine@patapon.info','just-perfection-desktop@just-perfection','logomenu@aryan_k','user-theme@gnome-shell-extensions.gcampax.github.com']
 
-enabled-extensions=[
-  'dash-to-panel@jderose9.github.com',
-  'app-hider@lynith.dev',
-  'extension-list@tu.berry',
-  'vitals@CoreCoding.com',
-  'appindicatorsupport@rgcjonas.gmail.com',
-  'blur-my-shell@aunetx',
-  'caffeine@patapon.info',
-  'just-perfection-desktop@just-perfection',
-  'logomenu@aryan_k',
-  'user-theme@gnome-shell-extensions.gcampax.github.com'
-]
-
 [org/gnome/shell/extensions/dash-to-panel]
 panel-position='BOTTOM'
 panel-fixed=true
