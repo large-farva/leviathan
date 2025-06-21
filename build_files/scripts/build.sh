99119034a4a3601b1b6f2fad21c96b1cb4d1c38e
large-farva <148006495+large-farva@users.noreply.github.com>
2025-06-21 13:12:10 -0700

    THEMING

--- build_files/scripts/build.sh
+++ build_files/scripts/build.sh
@@ -1,32 +1,17 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# 0. Locate our scripts directory
 SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-# 0.1 Derive the repo root (so branding.sh can find /branding)
 export SRCROOT="$(cd "${SCRIPTS_DIR}/../.." && pwd)"
 
-# 1. Remove unwanted RPM packages
 "$SCRIPTS_DIR/remove_packages.sh"
-
-# 2. Remove unwanted Flatpaks
 "$SCRIPTS_DIR/remove_flatpaks.sh"
-
-# 3. Install RPM packages
-"$SCRIPTS_DIR/install_packages.sh"
-
-# 4. Install Flatpaks
-"$SCRIPTS_DIR/install_flatpaks.sh"
-
-# 5. Remove unwanted GNOME Shell extensions
 "$SCRIPTS_DIR/remove_extensions.sh"
 
-# 6. Install GNOME Shell extensions
+"$SCRIPTS_DIR/install_packages.sh"
+"$SCRIPTS_DIR/install_flatpaks.sh"
 "$SCRIPTS_DIR/install_extensions.sh"
 
-# 7. Apply all dconf/GNOME settings (wallpaper, theme defaults, favorites, etc.)
-"$SCRIPTS_DIR/configure_dconf.sh"
-
-# 8. Copy in all your branding assets (wallpapers, icons, GTK/Shell themes)
 "$SCRIPTS_DIR/branding.sh"
+
+"$SCRIPTS_DIR/configure_dconf.sh"
--- build_files/scripts/configure_dconf.sh
+++ build_files/scripts/configure_dconf.sh
@@ -6,6 +6,10 @@ set -euxo pipefail
 cursor-size=24
 color-scheme='prefer-dark'
 clock-show-date=true
+icon-theme='Tela-nord'
+
+[org/gnome/desktop/background]
+picture-uri='file:///usr/share/backgrounds/black-windows.jpg'
 
 [org/gnome/desktop/wm/preferences]
 focus-mode='click'
@@ -15,6 +19,9 @@ auto-raise=true
 enable-hot-corners=false
 favorite-apps=['org.mozilla.firefox.desktop','steam.desktop','org.gnome.TextEditor.desktop','org.gnome.Nautilus.desktop','org.gnome.Calculator.desktop','gnome-software.desktop','gnome-control-center.desktop']
 enabled-extensions=['dash-to-panel@jderose9.github.com','app-hider@lynith.dev','extension-list@tu.berry','vitals@CoreCoding.com','appindicatorsupport@rgcjonas.gmail.com','blur-my-shell@aunetx','caffeine@patapon.info','just-perfection-desktop@just-perfection','logomenu@aryan_k','user-theme@gnome-shell-extensions.gcampax.github.com']
+
+[org/gnome/shell/extensions/user-theme]
+name='Leviathan'
 
 [org/gnome/shell/extensions/dash-to-panel]
 panel-position='BOTTOM'
