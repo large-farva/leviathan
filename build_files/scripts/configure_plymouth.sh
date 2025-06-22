22f0ea26aac50491c8731c555688688eeb2fbd11
large-farva <148006495+large-farva@users.noreply.github.com>
2025-06-22 13:28:27 -0700

    configure_plymouth.sh

--- build_files/scripts/configure_plymouth.sh
+++ build_files/scripts/configure_plymouth.sh
@@ -8,5 +8,3 @@ rm -rf /usr/share/plymouth/themes/charge
 install -Dm0644 \
   "${SRCROOT}/theming/plymouth/watermark.png" \
   /usr/share/plymouth/themes/spinner/watermark.png
-
-plymouth-set-default-theme bgrt -R
