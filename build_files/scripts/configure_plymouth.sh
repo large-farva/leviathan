e54de874c4c99792ffaf40b33f9bc79fefeda9e2
large-farva <sebastianvencill@gmail.com>
2025-09-30 08:03:36 +0300

    fix:configure_plymouth

--- build_files/scripts/configure_plymouth.sh
+++ build_files/scripts/configure_plymouth.sh
@@ -8,4 +8,9 @@ rm -rf /usr/share/plymouth/themes/charge
 install -d /usr/share/plymouth/themes/spinner_alt
 cp -r "${SRCROOT}/theming/plymouth/spinner_alt/"* /usr/share/plymouth/themes/spinner_alt/
 
-plymouth-set-default-theme -R spinner_alt
+cat >/etc/plymouth/plymouthd.conf <<EOF
+[Daemon]
+Theme=spinner_alt
+ShowDelay=0
+DeviceTimeout=8
+EOF
