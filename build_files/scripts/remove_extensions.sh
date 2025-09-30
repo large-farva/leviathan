5fc8f2a5ee036c88311955ff3d89dac98815a76b
large-farva <sebastianvencill@gmail.com>
2025-09-30 04:47:16 +0300

    fix: remove_extensions.sh

--- build_files/scripts/remove_extensions.sh
+++ build_files/scripts/remove_extensions.sh
@@ -2,15 +2,6 @@
 set -euxo pipefail
 
 rpm-ostree override remove \
-    # Bazzite Base
-    # gnome-shell-extension-burn-my-windows \
-    # gnome-shell-extension-compiz-alike-magic-lamp-effect \
-    # gnome-shell-extension-compiz-window-effect \
-    # gnome-shell-extension-desktop-cube \
-    # gnome-shell-extension-hanabi \
-    # gnome-shell-extension-hotedge \
-    # gnome-shell-extension-tilingshell || true
-    # Bluefin Base
     gnome-shell-extension-appindicator \
     gnome-shell-extension-dash-to-dock \
     gnome-shell-extension-gsconnect \
